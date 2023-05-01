0aa3c835263748ee1330a28561fcfde19ad162ea
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-05-01 10:04:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ function updateOutput() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('Hello, world!');
+        }, 1000);
+      });
+    }
+
+    updateOutput()
+      .then((message) => {
+        document.getElementById('output').textContent = message;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
